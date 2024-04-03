35ec8de198528f1020de656b4f685be2d682f024
Nahuel Beni <nsznaj@gmail.com>
2024-04-03 20:40:18 -0300

    set a timeout in the request

--- timeout_nojsrendernoproxy.py
+++ timeout_nojsrendernoproxy.py
@@ -1,23 +1,18 @@
-#  Request with javascript rendering, premium proxy and proxy country, 
-# Set a case if there's an error or not.
-# Set a retry logic for timeout
+# Request with javascript rendering, premium proxy and proxy country, 
+# Set a logic for timeout
 #
-# pip install zenrows
-#from zenrows import ZenRowsClient
+# 
 import os 
 from dotenv import load_dotenv
 load_dotenv()
-
-zenrowsApiKey = os.getenv("api_key")
-
-
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
+zenrowsApiKey = os.getenv("api_key")
+
 apikey = zenrowsApiKey
 urls = ['https://scrapeme.live/']
-
 
 zenrows_api_base = "https://api.zenrows.com/v1/"
 
